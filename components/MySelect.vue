8916f18e6cc673e8b2224ffc0e71b971eb06f168
ricky <522011796@qq.com>
2020-10-12 23:12:58 +0800

    init

--- components/MySelect.vue
+++ components/MySelect.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-    <el-select v-model="value" v-bind="selectModel" :placeholder="placeholder" :size="size" :clearable="clearable" :disabled="disabled" :filterable="filterable" @change="handleChange">
-      <el-option
-        v-for="item in options"
-        :key="item.value"
-        :label="item.label"
-        :value="item.value"
-        :disabled="item.disabled">
-      </el-option>
+    <el-select
+      v-model="value"
+      v-bind="selectModel"
+      :placeholder="placeholder"
+      :size="size"
+      :clearable="clearable"
+      :disabled="disabled"
+      :filterable="filterable"
+      :loading="loading"
+      :remote="remote"
+      :remote-method="remoteMethod"
+      @change="handleChange">
+
+      <template v-if="group == false">
+        <el-option
+          v-for="item in options"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+          :disabled="item.disabled">
+        </el-option>
+      </template>
+
+      <template v-if="group == true">
+        <el-option-group
+          v-for="itemGroup in options"
+          :key="itemGroup.label"
+          :label="itemGroup.label">
+          <el-option
+            v-for="itemChild in itemGroup.options"
+            :key="itemChild.value"
+            :label="itemChild.label"
+            :value="itemChild.value">
+          </el-option>
+        </el-option-group>
+      </template>
     </el-select>
   </div>
 </template>
@@ -48,6 +76,24 @@
       filterable: {
         default: false,
         type: Boolean
+      },
+      remote: {
+        default: false,
+        type: Boolean
+      },
+      loading: {
+        default: false,
+        type: Boolean
+      },
+      group: {
+        default: false,
+        type: Boolean
+      },
+      remoteMethod: {
+        default: function (data) {
+          return data;
+        },
+        type: Function
       }
     },
     computed: {
@@ -62,6 +108,7 @@
     },
     methods: {
       handleChange(data) {
+        console.log(33,data);
         this.$emit('change', data);
       }
     }
--- pages/test/test1.vue
+++ pages/test/test1.vue
@@ -4,7 +4,7 @@
     <div>
       <my-pagination :total="total" :current-page="page" :page-size="num" @currentPage="handleCurrentChangeExp" @sizeChange="handleSizeChangeExp"></my-pagination>
       <div>
-        <my-select class="pull-left" :sel-value="selModel" :options="options" :clearable="true" :disabled="false" :filterable="true" @change="handleChangeExp"></my-select>
+        <my-select class="pull-left" :sel-value="selModel" :options="options" :clearable="true" :disabled="false" :filterable="true" :remote="true" :loading="loading" :group="group" @change="handleChangeExp" :remote-method="remoteMethodExp"></my-select>
         <button class="pull-left" type="button" @click="changeSelect">set</button>
         <div class="moon-clearfix"></div>
       </div>
@@ -23,11 +23,22 @@
     data(){
       return {
         options: [],
-        selModel: ''
+        selModel: '',
+        loading: false,
+        group: true
       }
     },
     created() {
       this.init();
+      if (this.group){
+        console.log(21);
+        this.getSelectGroup();
+      }
+
+      if (!this.group){
+        console.log(12);
+        this.getSelect();
+      }
     },
     methods: {
       init(){
@@ -40,7 +51,22 @@
         this.num = data;
       },
       handleChangeExp(data){
+        console.log(11111,data);
         this.selModel = data;
+      },
+      remoteMethodExp(query){
+        this.loading = true;
+        if (query !== '') {
+          setTimeout(() => {
+            this.loading = false;
+            this.options = this.options.filter(item => {
+              return item.label.indexOf(query) > -1;
+            });
+          }, 200);
+        }else {
+          this.loading = false;
+          this.getSelect();
+        }
       },
       changeSelect(){
         this.getSelect("2");
@@ -64,8 +90,26 @@
               });
             }
           }
-          this.selModel = "2";
-        },2000);
+          //this.selModel = "2";
+        },100);
+      },
+      getSelectGroup(){
+        setTimeout(() => {
+          this.options = [];
+          for (let i = 0; i < 2; i++){
+            this.options.push({
+              label: '分组' + i,
+              options: []
+            });
+            for (let j = 0; j < 2; j++){
+              this.options[i].options.push({
+                label: '项目' + j,
+                value: '' + j
+              });
+            }
+          }
+          //this.selModel = "1";
+        },100);
       }
     }
   }
