7e306cca7eb0090a4fb360b51dbce6ba93ad1c1e
ricky <522011796@qq.com>
2021-01-16 11:09:06 +0800

    1、修改框架和组件样式、细节bug等 2、教学排课接口对接完成

--- pages/index.vue
+++ pages/index.vue
@@ -217,10 +217,198 @@
               </el-row>
             </div>
           </el-card>
-        </el-col>
-        <el-col :span="8">
-          <el-card :body-style="{padding: '0px'}" style="height: 140px">
 
+          <el-tabs type="border-card" class="margin-top-20" style="height: 400px">
+            <el-tab-pane label="通知"></el-tab-pane>
+            <el-tab-pane label="公告"></el-tab-pane>
+          </el-tabs>
+        </el-col>
+        <el-col :span="8" v-if="userType == 4">
+          <el-card :body-style="{padding: '0px'}" style="height: 140px">
+            <div slot="header" class="moon-clearfix padding-tb-10 padding-lr-10">
+              <span class="color-muted" style="font-weight: bold;position: relative; top: 0px;">
+                <i class="fa fa-line-chart"></i>
+                <span>{{$t("设备统计")}}</span>
+              </span>
+              <span class="pull-right color-grand">
+                0{{$t("台")}}
+              </span>
+              <div class="moon-clearfix"></div>
+            </div>
+            <div class="color-muted padding-tb-10 padding-lr-10" style="position: relative">
+              <el-row :gutter="16">
+                <el-col :span="8">
+                  <div class="item-attend-block">
+                    <div>{{$t("一体机本地版")}}</div>
+                    <div>
+                      <span class="font-size-25 color-grand">0</span>
+                    </div>
+                  </div>
+                </el-col>
+                <el-col :span="8">
+                  <div class="item-attend-block">
+                    <div>{{$t("一体机云端版")}}</div>
+                    <div>
+                      <span class="font-size-25 color-grand">0</span>
+                    </div>
+                  </div>
+                </el-col>
+                <el-col :span="8">
+                  <div class="item-attend-block">
+                    <div>{{$t("门禁控制器")}}</div>
+                    <div>
+                      <span class="font-size-25 color-grand">0</span>
+                    </div>
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+          </el-card>
+          <el-card :body-style="{padding: '0px'}" class="margin-top-20" style="height: 500px;overflow-y: auto">
+            <div slot="header" class="moon-clearfix padding-tb-10 padding-lr-10">
+              <span class="color-muted" style="font-weight: bold;position: relative; top: 5px;">
+                <i class="fa fa-line-chart"></i>
+                <span>{{$t("设备状态")}}</span>
+              </span>
+            </div>
+            <div class="color-muted padding-tb-10 padding-lr-10" style="position: relative">
+              <el-timeline>
+                <el-timeline-item :hide-timestamp="true" timestamp="2018/4/12" placement="top">
+                  <el-card class="font-size-12" :body-style="{padding: '10px'}">
+                    <div class="color-muted">
+                      <span class="color-grand">xxxxx设备</span>
+                      <el-tag size="mini" type="danger" class="margin-left-5">{{$t("离线")}}</el-tag>
+                      <el-tag size="mini" type="info" class="margin-left-5">{{$t("一体机本地版")}}</el-tag>
+                    </div>
+                    <div class="margin-top-5">
+                      <span class="color-disabeld">{{$t("SN")}}: xxxxxxxxx</span>
+                    </div>
+                    <div class="margin-top-5">
+                      <span class="color-muted">{{$t("位置")}}: xxxxxxxxx</span>
+                    </div>
+                    <div class="margin-top-5">
+                      <i class="fa fa-times-circle"></i>
+                      <span class="color-disabeld">2011-11-11 11:11:11</span>
+                    </div>
+                  </el-card>
+                </el-timeline-item>
+              </el-timeline>
+            </div>
+          </el-card>
+        </el-col>
+
+        <el-col :span="8" v-if="userType == 5">
+          <el-card :body-style="{padding: '0px'}" style="height: 140px">
+            <div slot="header" class="moon-clearfix padding-tb-10 padding-lr-10">
+              <span class="color-muted" style="font-weight: bold;position: relative; top: 0px;">
+                <i class="fa fa-line-chart"></i>
+                <span>{{$t("素质学分统计")}}</span>
+              </span>
+              <span class="pull-right color-grand">
+                <my-cascader ref="SelectorBuild" width-style="180" :clearable="true" size="small" :sel-value="collegeCreditData" type="1" sub-type="4" @change="handleCascaderChange($event,4)"></my-cascader>
+              </span>
+              <div class="moon-clearfix"></div>
+            </div>
+            <div class="color-muted padding-tb-10 padding-lr-10" style="position: relative">
+              <el-row :gutter="16">
+                <el-col :span="12">
+                  <div class="item-attend-block">
+                    <div>{{$t("加分人数")}}</div>
+                    <div>
+                      <span class="font-size-25 color-grand">0</span>
+                    </div>
+
+                    <div class="pull-right">
+                      <div class="credit-title add">
+                        {{$t("加")}}
+                      </div>
+                    </div>
+                    <div class="moon-clearfix"></div>
+                  </div>
+                </el-col>
+                <el-col :span="12">
+                  <div class="item-attend-block">
+                    <div>{{$t("减分人数")}}</div>
+                    <div>
+                      <span class="font-size-25 color-grand">0</span>
+                    </div>
+
+                    <div class="pull-right">
+                      <div class="credit-title minus">
+                        {{$t("减")}}
+                      </div>
+                    </div>
+                    <div class="moon-clearfix"></div>
+                  </div>
+                </el-col>
+              </el-row>
+            </div>
+          </el-card>
+          <el-card :body-style="{padding: '0px'}" class="margin-top-20" style="height: 500px;overflow-y: auto" v-if="userType == 4">
+            <div slot="header" class="moon-clearfix padding-tb-10 padding-lr-10">
+              <span class="color-muted" style="font-weight: bold;position: relative; top: 5px;">
+                <i class="fa fa-line-chart"></i>
+                <span>{{$t("设备状态")}}</span>
+              </span>
+            </div>
+            <div class="color-muted padding-tb-10 padding-lr-10" style="position: relative">
+              <el-timeline>
+                <el-timeline-item :hide-timestamp="true" timestamp="2018/4/12" placement="top">
+                  <el-card class="font-size-12" :body-style="{padding: '10px'}">
+                    <div class="color-muted">
+                      <span class="color-grand">xxxxx设备</span>
+                      <el-tag size="mini" type="danger" class="margin-left-5">{{$t("离线")}}</el-tag>
+                      <el-tag size="mini" type="info" class="margin-left-5">{{$t("一体机本地版")}}</el-tag>
+                    </div>
+                    <div class="margin-top-5">
+                      <span class="color-disabeld">{{$t("SN")}}: xxxxxxxxx</span>
+                    </div>
+                    <div class="margin-top-5">
+                      <span class="color-muted">{{$t("位置")}}: xxxxxxxxx</span>
+                    </div>
+                    <div class="margin-top-5 color-disabeld">
+                      <i class="fa fa-clock-o"></i>
+                      <span>2011-11-11 11:11:11</span>
+                    </div>
+                  </el-card>
+                </el-timeline-item>
+              </el-timeline>
+            </div>
+          </el-card>
+
+          <el-card :body-style="{padding: '0px'}" class="margin-top-20" style="height: 500px;overflow-y: auto" v-if="userType == 5">
+            <div slot="header" class="moon-clearfix padding-tb-10 padding-lr-10">
+              <span class="color-muted" style="font-weight: bold;position: relative; top: 5px;">
+                <i class="fa fa-line-chart"></i>
+                <span>{{$t("素质学分动态")}}</span>
+              </span>
+            </div>
+            <div class="color-muted padding-tb-10 padding-lr-10" style="position: relative">
+              <el-timeline>
+                <el-timeline-item :hide-timestamp="true" timestamp="2018/4/12" placement="top">
+                  <el-card class="font-size-12" :body-style="{padding: '10px'}">
+                    <div class="color-muted">
+                      <span class="color-grand">xxxxx姓名</span>
+                      <el-tag size="mini" type="danger" class="margin-left-5">{{$t("加分")}}</el-tag>
+                      <el-tag size="mini" type="danger" class="margin-left-5">
+                        <i class="fa fa-tag"></i>
+                        1{{$t("分")}}
+                      </el-tag>
+                    </div>
+                    <div class="margin-top-5">
+                      <span class="color-disabeld">xxxxxxx班级}</span>
+                    </div>
+                    <div class="margin-top-5">
+                      <span class="color-muted">xxxxxxxxxxxxxxxxxx内容</span>
+                    </div>
+                    <div class="margin-top-5 color-disabeld">
+                      <i class="fa fa-clock-o"></i>
+                      <span>2011-11-11 11:11:11</span>
+                    </div>
+                  </el-card>
+                </el-timeline-item>
+              </el-timeline>
+            </div>
           </el-card>
         </el-col>
       </el-row>
@@ -242,8 +430,11 @@
         userType: 1,
         collegeData: [],
         collegeTabData: [],
+        collegeCreditData: [],
         deptData: [],
         tableData: [],
+        userType: '5',
+        activeName: 'notice',
         format(percentage) {
           return percentage === 100 ? '' : `${percentage}%`;
         }
@@ -267,8 +458,14 @@
           case 3:
             this.collegeTabData = data;
             break;
+          case 4:
+            this.collegeCreditData = data;
+            break;
         }
       },
+      handleClick(tab, event) {
+        console.log(tab, event);
+      }
     }
   }
 </script>
@@ -295,10 +492,26 @@
   height: 100%;
   padding: 10px 10px;
   border-radius: 5px;
+  position: relative;
 }
 .item-attend-line-ver{
   background: #f2f2f2;
   height:220px;
   width: 1px;
+}
+.credit-title{
+  font-size: 20px;
+  color: #FFFFFF;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 10px;
+  padding: 10px;
+}
+.add{
+  background: rgb(160, 207, 255);
+}
+.minus{
+  background: rgb(253, 226, 226);
 }
 </style>
