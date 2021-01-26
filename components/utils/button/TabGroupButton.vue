3ff21ca47c3d9d8a260a7473cf998ec4ec767be6
ricky <522011796@qq.com>
2021-01-26 11:25:46 +0800

    1、修改框架和组件样式、细节bug等 2、切换可动画版本的按钮组完成

---
+++ components/utils/button/TabGroupButton.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="btn-group-block select-none" :class="setClass" :style="setBgColor">
+    <div class="btn-group-item-active" :style="[transformBtnGroup,setActiveColor]"></div>
+    <div class="btn-group-item">
+      <div class="btn-group-item-default pull-left" v-for="(item, index) in options" :class="isActive == index ? 'is-active' : ''" :key="index" @click="handelChange($event , item, index)">{{item.label}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {oneOf} from '../../../utils/utils';
+  export default {
+    name: 'auditStatusButton',
+    props: {
+      options: {
+        default: function(){
+          return []
+        },
+        type : Array
+      },
+      size: {
+        defalult: 'medium',
+        validate: function (val) {
+          return oneOf(val, ['large', 'medium', 'small', 'mini']);
+        }
+      },
+      bgColor: {
+        default: '#409EFF',
+        type: String
+      },
+      activeColor: {
+        default: '#FFFFFF',
+        type: String
+      }
+    },
+    computed: {
+      setClass(){
+        return [
+          {
+            ['tab-group-button-medium']: !this.size,
+            [`tab-group-button-${this.size}`]: !!this.size,
+          }
+        ]
+      },
+      setBgColor(){
+        this.bgGroupColor.background = this.bgColor;
+        return this.bgGroupColor;
+      },
+      setActiveColor(){
+        this.activeGroupColor.background = this.activeColor;
+        return this.activeGroupColor;
+      }
+    },
+    data() {
+      return {
+        selVal: 1,
+        widthAll: 0,
+        isActive: '',
+        transformBtnGroup: {
+          transform: 'translateX(0px)',
+          transition: '0.5s',
+          width: '',
+        },
+        activeGroupColor:{
+          background: "#FFFFFF"
+        },
+        bgGroupColor:{
+          background: "#409EFF"
+        }
+      }
+    },
+    mounted() {
+      let widthAll = 0;
+      let translateX = widthAll + 5 + "px";
+      let groupItem = document.querySelectorAll(".btn-group-item-default");
+      this.transformBtnGroup.width = groupItem[0].clientWidth + "px";
+      this.transformBtnGroup.transform = 'translateX(' + translateX + ')';
+    },
+    created() {
+      //this.initConfig();
+    },
+    methods: {
+      handelChange(data, item, index) {
+        let padding = 10;
+        let widthAll = 0;
+        this.selVal = item;
+        let groupItem = document.querySelectorAll(".btn-group-item-default");
+        for (let i = 0; i < groupItem.length; i++) {
+          if (i < index) {
+            widthAll += groupItem[i].clientWidth;
+          }
+          if (index == i) {
+            this.transformBtnGroup.width = groupItem[i].clientWidth + "px";
+            console.log(groupItem[i].clientWidth);
+            break;
+          }
+        }
+        let translateX = widthAll + 5 + "px";
+        this.transformBtnGroup.transform = 'translateX(' + translateX + ')';
+        this.isActive = index;
+
+        this.$emit('click', item);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .btn-group-block{
+    padding: 10px 5px;
+    position:relative;
+    border-radius: 5px;
+    display: inline-block;
+    vertical-align: middle;
+    white-space: nowrap;
+    transition: transform .3s;
+    float: left;
+    z-index: 2;
+  }
+  .btn-group-item{
+    position: relative;
+  }
+  .btn-group-item-default{
+    padding: 0px 5px;
+    border-radius: 5px;
+    display: inline-block;
+    cursor: default;
+    color: #FFFFFF;
+  }
+  /*.btn-group-item-active{
+    position: absolute;
+    top: 5px;
+    left: 0px;
+    height: 30px;
+    transform: translateX(0px);
+    border-radius: 5px;
+  }*/
+  .is-active{
+    color: #595959;
+  }
+  .tab-group-button-medium{
+    padding: 12px 5px;
+    font-size: 12px;
+  }
+  .tab-group-button-small{
+    padding: 9px 5px;
+    font-size: 12px;
+  }
+  .tab-group-button-mini{
+    padding: 7px 5px;
+    font-size: 12px;
+  }
+  .tab-group-button-large{
+    padding: 14px 5px;
+  }
+  .tab-group-button-medium .btn-group-item-active{
+    position: absolute;
+    top: 5px;
+    left: 0px;
+    height: 30px;
+    transform: translateX(0px);
+    border-radius: 5px;
+  }
+  .tab-group-button-small .btn-group-item-active{
+    position: absolute;
+    top: 5px;
+    left: 0px;
+    height: 25px;
+    transform: translateX(0px);
+    border-radius: 5px;
+  }
+  .tab-group-button-mini .btn-group-item-active{
+    position: absolute;
+    top: 5px;
+    left: 0px;
+    height: 22px;
+    transform: translateX(0px);
+    border-radius: 5px;
+  }
+  .tab-group-button-large .btn-group-item-active{
+    position: absolute;
+    top: 8px;
+    left: 0px;
+    height: 34px;
+    transform: translateX(0px);
+    border-radius: 5px;
+  }
+</style>
--- pages/test/test4.vue
+++ pages/test/test4.vue
@@ -1,25 +1,49 @@
 <template>
-  <div class="container">
-    <div>test4</div>
+  <div class="container test-layout">
+    <div class="margin-top-5">
+      <tab-group-button size="small" :options="options" @click="handleClickTab"></tab-group-button>
+    </div>
   </div>
 </template>
 
 <script>
+  import TabGroupButton from "../../components/utils/button/TabGroupButton";
   export default {
+    layout: 'defaultFullScreen',
     name: 'test4',
+    components: {TabGroupButton},
     data(){
       return {
-
+        options: [
+          {
+            label: '全部',
+            value: ''
+          },
+          {
+            label: '待审核',
+            value: '1'
+          },
+          {
+            label: '未审核',
+            value: '2'
+          },
+          {
+            label: '全部通过',
+            value: '3'
+          }
+        ]
       }
     },
     methods: {
-
+      handleClickTab(data){
+        console.log(data);
+      }
     }
   }
 </script>
 
 <style scoped>
 .container {
-
+  padding: 10px 15px;
 }
 </style>
