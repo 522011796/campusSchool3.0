2125e9ae7b89b936d3702b427f17b5eab75d367f
ricky <ricky@netmoon.cn>
2023-05-16 15:40:47 +0800

    增加毕业和异动管理

--- pages/attendanceInfo/administration/setting.vue
+++ pages/attendanceInfo/administration/setting.vue
@@ -647,7 +647,7 @@
     },
     created() {
       this.init();
-      this.initDevice("1,2,4", "3,4", 1);
+      this.initDevice("1,2,3,4", "3,4", 1);
       this.initDevice("4", "6", 2);
     },
     methods: {
